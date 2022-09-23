14851f7a51b9cbed03f8030e19fc90b9b5c9af05
Alec <movieornasasim@gmail.com>
2022-09-23 16:13:26 -0500

    Created UtilityCommands
    A class that stores default commands.
    The Help Method will be used to list all of the available commands.

---
+++ Assets/Scripts/Console/Commands/DefaultCommands/UtilityCommands.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UtilityCommands
+{
+    public UtilityCommands()
+    {
+        CommandManager.AddCommand("help", "displays all commands", Help);
+    }
+
+    void Help()
+    {
+        Debug.Log("Display stuff");
+        //List all commands;
+    }
+}
