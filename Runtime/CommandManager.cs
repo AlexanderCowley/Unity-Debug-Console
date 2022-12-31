6b844755f08d68fd09ee0c34f396f2dbc62a6292
Alec <movieornasasim@gmail.com>
2022-12-31 13:13:48 -0600

    Added support for the addcommand attribute
    The command manager searches for methods with the add command attribute and gathers monobehavior instances to invoke commands.
    
    The GetUtilityCommands() method was added to gather the utility commands like help and clear. Gets the executing assembly, the UtilityCommands type and then loops through the methods and add them as commands through the addcommand attribute.
    
    The CreateDelegate commands creates a delegate based on the method signature using the MethodInfo and target.

--- Runtime/CommandManager.cs
+++ Runtime/CommandManager.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
+using Object = UnityEngine.Object;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace RuntimeDebugger.Commands
 {
@@ -15,6 +19,108 @@ namespace RuntimeDebugger.Commands
         public static IConsoleCommand LastCommand { get; private set; }
 
         public static List<string> InputCommandLogs = new List<string>();
+        
+        static AddCommandAttribute _cmdAttr;
+        public static void AddAllCommands()
+        {
+            //Iterate through each assembly to find assemblies with the AddCommandAttribute
+            //Assembly -> Types -> Methods -> Methods with Attribute
+            //Assembly -> Types (Get Types with attribute, Get Instances), -> Methods
+            //Assembly -> Types -> Types with attributes -> instances -> methods with attribute
+            /*Where statements blacklist assemblies that starts with the name
+            Unity, Microsoft and System, reduces the assembly to loop from 157 to 15
+            in a mostly empty project that holds up to three assemblies for each tool directory
+            */
+            //Grabs utility commands
+            GetUtilityCommands();
+            //Get Monobehavior Instance
+            MonoBehaviour[] monoBehaviours = GetMonoBehaviours(true);
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies().
+            Where(x => !x.FullName.StartsWith("Unity") && 
+            !x.FullName.StartsWith("Microsoft") &&
+            !x.FullName.StartsWith("System"));
+            for(int i = 0; i < assemblies.Count(); i++)
+            {
+                Type[] types = assemblies.ElementAt(i).GetTypes();
+                //Switch to memberinfo when adding property and field support
+                for(int j = 0; j < types.Length; j++)
+                {
+                    MethodInfo[] methods = types[j].
+                        GetMethods(BindingFlags.Public | 
+                        BindingFlags.NonPublic | BindingFlags.Static |
+                        BindingFlags.Instance);
+                    var monos = monoBehaviours.Where(x => x.GetType() == types[j]);
+
+                    for(int k = 0; k < methods.Length; k++)
+                    {
+                        //Check types for attribute
+                        _cmdAttr = methods[k].GetCustomAttribute<AddCommandAttribute>();
+                        if(_cmdAttr == null)
+                            continue;
+
+                        for(int l = 0; l < monos.Count(); l++)
+                        {
+                            //Get Delegate type
+                            Delegate cmd = CreateDelegate(methods[k], monos.ElementAt(l));
+                            new ConsoleCommand(_cmdAttr.CmdName, _cmdAttr.CmdDesc, cmd, 
+                            monos.ElementAt(l));
+                        }
+                        //Get object Instance from register object
+                        //Remove object Instance from unregister object
+                    }
+                }
+            }
+            //Retrieve attribute for attribute data from parameters
+            //Gather all methods with the AddCommand Attribute
+            //Create Instance of new command
+            //Create a new delegate the new command
+            //Add them to the dictionary using the <command><methodInfo>
+
+            static void GetUtilityCommands()
+            {
+            //Get utility commands
+            var UtilityCommands = Assembly.
+                GetExecutingAssembly().GetType("RuntimeDebugger.Commands.UtilityCommands").
+                GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+                for(int i = 0; i < UtilityCommands.Count(); i++)
+                {
+                    _cmdAttr = UtilityCommands.ElementAt(i).
+                        GetCustomAttribute<AddCommandAttribute>();
+
+                    if(_cmdAttr == null)
+                        continue;
+
+                    Delegate cmd = CreateDelegate(UtilityCommands.ElementAt(i), null);
+                    new ConsoleCommand(_cmdAttr.CmdName, _cmdAttr.CmdDesc, cmd);
+                }
+            }
+        }
+
+        static Delegate CreateDelegate(MethodInfo methodInfo, object target)
+        {
+            Func<Type[], Type> getType;
+            bool isAction = methodInfo.ReturnType.Equals(typeof(void));
+            var types = methodInfo.GetParameters().Select(p => p.ParameterType);
+            
+            if(isAction) getType = Expression.GetActionType;
+            else
+            {
+                getType = Expression.GetFuncType;
+                types = types.Concat(new[]{methodInfo.ReturnType});
+            }
+
+            if(methodInfo.IsStatic)
+                return Delegate.CreateDelegate(getType(types.ToArray()), methodInfo);
+
+            return Delegate.CreateDelegate(getType(types.ToArray()), target, methodInfo.Name);;
+        }
+
+        public static MonoBehaviour[] GetMonoBehaviours(bool allowInActive)
+        {
+            MonoBehaviour[] monoBehaviours = Object.FindObjectsOfType<MonoBehaviour>(allowInActive);
+            return monoBehaviours;
+        }
 
         public static void AddCommand(string commandTitle, string commandDescription, Action command)
         {
@@ -25,7 +131,6 @@ namespace RuntimeDebugger.Commands
 #endif
                 return;
             }
-            new ConsoleCommand(commandTitle, commandDescription, command);
         }
 
         public static void AddCommand<T>(string commandTitle, string commandDescription,
